969eb01aadda0f29e69d347abe25685d1f04160e
acmota2 <acmota2@gmail.com>
2022-11-18 14:22:42 +0000

    [CLIENT and IP]: IP = finished; Client needs parsing. All needs revision

--- CL/client.py
+++ CL/client.py
@@ -1,27 +1,43 @@
 import sys
+import common.ip as ip
 
 def main(argv):
     help = '''
 dnscl IP[:port] DOMAIN [ARGS]
 
 ARGS can be:
-R   Recursive query execution. This is off by default
+R
+    Recursive query execution. This is off by default.
 
-MX  
+MX
+    Specifies an e-mail server for the domain mail indicated.
+
+A
+    Specifies an IPv4 address of a host/server indicated by argument as a name.
+    This supports priorities.
+
+CNAME
+    Specifies an canonic name (or alias) associated to the name given in argument.
+
+PTR
+    Specifies a name of a server/host using the IPv4 used presented as argument.
 '''
-    possible_args = {
-        'R': execute_recursive(),
-        'MX': ...,
+    is_ip, has_port = ip.check_ip(argv[1])
+    if len(argv) >= 5 and is_ip:
+        ip = ip.IP(argv[1], has_port)
+        
+        # verificar se isto precisam de ser funções
+        possible_args = set(['R', 'MX', 'NS', 'A', 'PTR', 'CNAME'])
 
-    }
+        for arg in argv[3:]:
+            if not arg not in possible_args:
+                break
 
-    if len(argv) < 5:
-        print(help)
+        # query parsing to send goes here
 
-def execute_recursive():
-    ...
+    print(help)
 
-def 
+    # vtable de args
 
 if __name__ == "__main__":
     main(sys.argv)
