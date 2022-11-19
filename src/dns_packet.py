2c312c020dda9868a462e8052081ce62fbcd74d0
Bernard A. Georges <73658047+BernardMatulo@users.noreply.github.com>
2022-11-19 18:51:21 +0000

    foi feito os encodes e mudança de nome

--- src/dns_packet.py
+++ src/dns_packet.py
@@ -1,15 +1,18 @@
 import random
 
-class DNS:
+class dns_packet:
 
-    def utf8len(s):
-        return len(s.encode('utf-8'))
+    def encodePacket(self,s):
+        return s.encode('utf-8')
+
+    def decodePacket(self,s):
+        return s.decode('utf-8')
     # Flags - 0 - Q; 1 - R; 10 - A ; 1111 - nulo
     def __init__(self,responseCode,numValues,numAuths,numExtra,responseValues,authValues,extraValues,flags = 1111 ,msgID = random.randint(1,65535),queryInfo = None):
         msg = msgID.__str__()
         msg ++ flags.__str__()
         dataFields = responseCode ++ numValues ++ numAuths ++ numExtra
-        if self.utf8len(dataFields) > 1000:
+        if len(self.enconde(dataFields)) > 1000:
             print("data is to big")
             return -1
         if responseCode in [1,2,3] and numValues < 255 and numAuths < 255 and numExtra < 255:
