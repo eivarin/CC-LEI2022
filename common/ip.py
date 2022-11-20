ae026364d4d27066b241d22a44cea78ce5eb203b
André Mota <48682058+acmota2@users.noreply.github.com>
2022-11-20 18:10:57 +0000

    [IP]: cria portas random agora (#11)
    * [IP]: cria portas random agora
    
    * [IP]: in and out correctly defined
    
    * Handler de pacotes tcp
    
    * UDP Working, TCP not Working needs rethinking
    
    * foi feita o DNS header (#7)
    
    * foi feita o DNS header
    
    * foi feito os encodes e mudança de nome
    
    * retirei o ideia
    
    * pos o .ideia no gitignore
    
    * mudança nos encode e decodes
    
    * coreção erro
    
    * Handler de pacotes tcp
    
    * [Client]: Started client
    
    * [Common]: new ip module
    
    * [CLIENT and IP]: IP = finished; Client needs parsing. All needs revision
    
    * [IP]: corrected RegEx and PR review stuff
    
    * UDP Working, TCP not Working needs rethinking
    
    Co-authored-by: eivarin <rodrigommpereira309@gmail.com>
    Co-authored-by: acmota2 <acmota2@gmail.com>
    
    * [IP]: cria portas random agora
    
    * [IP]: in and out correctly defined
    
    * Handler de pacotes tcp
    
    * UDP Working, TCP not Working needs rethinking
    
    * foi feita o DNS header (#7)
    
    * foi feita o DNS header
    
    * foi feito os encodes e mudança de nome
    
    * retirei o ideia
    
    * pos o .ideia no gitignore
    
    * mudança nos encode e decodes
    
    * coreção erro
    
    * Handler de pacotes tcp
    
    * [Client]: Started client
    
    * [Common]: new ip module
    
    * [CLIENT and IP]: IP = finished; Client needs parsing. All needs revision
    
    * [IP]: corrected RegEx and PR review stuff
    
    * UDP Working, TCP not Working needs rethinking
    
    Co-authored-by: eivarin <rodrigommpereira309@gmail.com>
    Co-authored-by: acmota2 <acmota2@gmail.com>
    
    Co-authored-by: eivarin <rodrigommpereira309@gmail.com>
    Co-authored-by: Bernard A. Georges <73658047+BernardMatulo@users.noreply.github.com>

--- common/ip.py
+++ common/ip.py
@@ -1,44 +1,37 @@
 import ctypes as C
 from re import fullmatch
+from random import randrange
 
-# data IPv4 = IPPort { a :: Word8, b :: Word8, c :: Word8, d :: Word8, e :: Word16 } | IP { a :: Word8, b :: Word8, c :: Word8, d :: Word8 }
+# data IPv4 = IPPort { a :: Word8, b :: Word8, c :: Word8, d :: Word8, e :: Word16 } | 
+#             IP { a :: Word8, b :: Word8, c :: Word8, d :: Word8 }
 
 class IP:
-    def __init__(self, ip: str, has_port = False):
-        self.has_port = has_port
-        port = None
-        if has_port:
+    def __init__(self, ip: str, port = None, has_port = False):
+        port = randrange(1025, 65535)
+
+        if has_port and port != None:
             ip, port = ip.split(':')
+            port = int(port)
 
         ip = [int(i) for i in ip.split('.')]
-        ip_tmp = (
+        self.ip = (
             C.c_uint8(ip[0]),
             C.c_uint8(ip[1]),
             C.c_uint8(ip[2]),
-            C.c_uint8(ip[3])
+            C.c_uint8(ip[3]),
+            C.c_uint16(port)
         )
-        a, b, c, d = ip_tmp
-        if port == None:
-            self.ip = ip_tmp 
-        else:
-            self.ip = a, b, c, d, C.c_uint16(int(port))
 
     def __str__(self):
-        if self.has_port:
-            a,b,c,d,e = self.ip
-            return f'{a.value}.{b.value}.{c.value}.{d.value}:{e.value}'
-        a,b,c,d = self.ip
-        return f'{a.value}.{b.value}.{c.value}.{d.value}'
+        a,b,c,d,e = self.ip
+        return f'{a.value}.{b.value}.{c.value}.{d.value}:{e.value}'
 
     def __repr__(self):
         return self.__str__()
 
-    def ip_value_tuple(self):
-        ip_tmp = str(self)
-        if self.has_port:
-            ip, port = ip_tmp.split(':')
-            return ip, int(port)
-        return ip_tmp
+    def ip_tuple(self):
+        ip, port = str(self).split(':')
+        return ip, int(port)
 
 def check_ip(ip):
     ip_match = fullmatch(r'^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d).?\b){4}(\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5])?$', ip)
